420634f9662ce7fcb9bf5b6ed8221b93df279677
chenyulin <chenyulin@sjtu.edu.cn>
2024-06-05 22:01:12 +0800

    fix. work on win11

--- predict.py
+++ predict.py
@@ -75,16 +75,16 @@ def main(args):
     cmd=[args.ffmpeg, '-y', '-framerate','30', '-r', '30',
          '-i', f'{output_dir}/images_out_smooth/%d-bbox.jpg',
          '-c:v', 'libx264','-pix_fmt','yuv420p', f'{output_dir}/video.mp4']
-    subprocess.run(cmd)
+    #subprocess.run(cmd)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='configs/gen6d_pretrain.yaml')
-    parser.add_argument('--database', type=str, default="custom/mouse")
-    parser.add_argument('--output', type=str, default="data/custom/mouse/test")
+    parser.add_argument('--database', type=str, default="custom/mouse_processed")
+    parser.add_argument('--output', type=str, default="data/custom/mouse_processed/test")
 
     # input video process
-    parser.add_argument('--video', type=str, default="data/custom/video/mouse-test.mp4")
+    parser.add_argument('--video', type=str, default="mouse-test.mp4")
     parser.add_argument('--resolution', type=int, default=960)
     parser.add_argument('--transpose', action='store_true', dest='transpose', default=False)
 
